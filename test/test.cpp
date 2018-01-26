31d06b690af5088405179582acf482801e0cd0e4
Andreas Walz <andreas.walz@hs-offenburg.de>
2018-01-26 14:07:10 +0100

    added tests

---
+++ test/test.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include "CompositeDataUnit.h"
+
+using std::cout;
+using std::endl;
+
+
+int main(int argc , char *argv[]) {
+
+    CompositeDataUnit cdu;
+
+    cdu.print();
+
+	return EXIT_SUCCESS;
+}
+
+
+
